37235f92a4db25671c5196aff8ab6295359d8651
Gyani Sunkara <gyanisunkara1@gmail.com>
2023-08-08 04:24:09 +0530

    Circularly linked list implementation

--- Run.java
+++ Run.java
@@ -1,3 +1,4 @@
+import ds.linkedlist.CircularlyLinkedList;
 import ds.linkedlist.SinglyLinkedList;
 import ds.queue.ArrayQueue;
 import ds.queue.LinkedQueue;
@@ -39,13 +40,29 @@ public class Run {
          * ArrayQueue & LinkedQueue
          */
         // ArrayQueue<Integer> queue = new ArrayQueue<>(2);
-        LinkedQueue<Integer> queue = new LinkedQueue<>();
+        // LinkedQueue<Integer> queue = new LinkedQueue<>();
 
-        queue.enqueue(3);
-        queue.dequeue();
-        queue.enqueue(88);
-        System.out.println(queue.first());
-        System.out.println(queue.size());
-        System.out.println(queue.isEmpty());
+        // queue.enqueue(3);
+        // queue.dequeue();
+        // queue.enqueue(88);
+        // System.out.println(queue.first());
+        // System.out.println(queue.size());
+        // System.out.println(queue.isEmpty());
+
+        /**
+         * CircularlyLinkedList
+         */
+        CircularlyLinkedList<Character> list = new CircularlyLinkedList<>();
+
+        list.addFirst('A');
+        list.addFirst('B');
+        list.addLast('C');
+        list.rotate();
+        System.out.println(list.isEmpty());
+        System.out.println(list.size());
+        System.out.println(list.first());
+        System.out.println(list.last());
+        System.out.println(list.removeFirst());
+        System.out.println(list.first());
     }
 }
