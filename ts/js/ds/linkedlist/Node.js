d49a354bb0e1bf40a89df358f247c7f6adc6ab3c
Gyani Sunkara <gyanisunkara1@gmail.com>
2023-09-12 12:55:25 -0400

    Remove js source files

--- ts/js/ds/linkedlist/Node.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Node {
-    constructor(value, next = null) {
-        this.value = value;
-        this.next = next;
-    }
-}
-exports.default = Node;
